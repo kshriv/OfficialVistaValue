f23b5aa875e61edf700d3817497ed115e4b6852d
Ari Jain <thearijain@gmail.com>
2020-05-19 22:49:55 -0400

    finished the ui table view controller completed still working on updating total expense label

--- OfficialVistaValue/View Controllers/ListViewViewController.swift
+++ OfficialVistaValue/View Controllers/ListViewViewController.swift
@@ -43,40 +43,12 @@ extension ListViewViewController : UITableViewDataSource, UITableViewDelegate {
     }
     
     //Allows you to swipe to delete
-//    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//        let delete = deleteRow(at: indexPath)
-//        return UISwipeActionsConfiguration(actions: [delete])
-//
-//    }
-//
-//    func deleteRow(at indexPath: IndexPath) -> UIContextualAction {
-//        deletePersistData(at: chargeArray[indexPath.row])
-//
-//        print("THIS IS THE CHARGE GETTING DELETED", chargeArray[indexPath.row])
-//        print("INDEXPATH . ROW", indexPath.row)
-//
-//        let action =  UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
-//            chargeArray.remove(at: indexPath.row)
-//            self.tableView.deleteRows(at: [indexPath], with: .automatic)
-//            completion(true)
-//        }
-////        persistChargeArray(chargeArray)
-//
-//        print("CHARGE ARRAY: \n", chargeArray)
-//
-//
-//        action.backgroundColor = .red
-//        return action
-//    }
-//
-    
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
-        print("delete buttton actually pressed")
         deletePersistData(at: chargeArray[indexPath.row])
 
         if editingStyle == .delete {
@@ -105,18 +77,16 @@ extension ListViewViewController : UITableViewDataSource, UITableViewDelegate {
     
     func deletePersistData(at charge: Charge) {
         let previousAmount = defaults.double(forKey: charge.category)
-        print("PREVIOUS AMOUNT OF THE CHARGE GETTING DELETED", previousAmount)
         defaults.set(previousAmount - charge.amount, forKey: charge.category)
-        print("AMOUNT IN THE CATEGOREY AFTER  BEING DELETED", defaults.double(forKey: charge.category))
         //Don't forget to update the total expense label as well !
 
     }
     
     func setChargeArray() -> [Charge] {
-           guard let encodedData = defaults.array(forKey: UserDefaultKey.chargeArray) as? [Data] else {
-               return []
-           }
-           return encodedData.map { try! JSONDecoder().decode(Charge.self, from: $0)}
+        guard let encodedData = defaults.array(forKey: UserDefaultKey.chargeArray) as? [Data] else {
+           return []
+        }
+        return encodedData.map { try! JSONDecoder().decode(Charge.self, from: $0)}
     }
        
     func persistChargeArray(_ arrayOfCharges: [Charge]) {
