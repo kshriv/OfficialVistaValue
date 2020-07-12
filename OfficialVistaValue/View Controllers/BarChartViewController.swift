3a8b3c000778e145cb34ce1734ba36fb82d2ef98
Komal <komal.shri1999@gmail.com>
2020-07-11 23:34:38 -0400

    colors for line graph

--- OfficialVistaValue/View Controllers/BarChartViewController.swift
+++ OfficialVistaValue/View Controllers/BarChartViewController.swift
@@ -16,16 +16,14 @@ class BarChartViewController: UIViewController, ChartViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         lineChartView.rightAxis.enabled = false
+        lineChartView.isUserInteractionEnabled = true
         setData()
-
     }
     
-    //im a god for not the logic but the face that it worked in one compile
+    //im a God for not the logic but the fact that it worked in one compile
     func setData() {
         setAllArrays()
-
         
         var lineChartDataSetArr = [IChartDataSet]()
         
@@ -39,25 +37,35 @@ class BarChartViewController: UIViewController, ChartViewDelegate {
                 }
                 if (chartEntries.count != 0) {
                     let set = LineChartDataSet(entries: chartEntries, label: category)
-                    set.drawCirclesEnabled = false
+                    let color = randomColor()
+                    set.drawCirclesEnabled = true
                     set.mode = .cubicBezier
                     set.lineWidth = 2
+                    set.setColor(color)
+                    set.setCircleColor(color)
                     lineChartDataSetArr.append(set)
                 }
             }
         }
-        
+
         let data = LineChartData(dataSets: lineChartDataSetArr)
         lineChartView.data = data
         
     }
     
-
     func convertDate(date: Date) -> Double {
         let month = Calendar.current.dateComponents([.month], from: date).month!
         let day = Calendar.current.dateComponents([.day], from: date).day!
         let dateDouble = (Double)(month) + (((Double)(day)) / 100.0)
         return dateDouble
+    }
+    
+    private func randomColor() -> UIColor {
+        let red = Double(arc4random_uniform(256))
+        let green = Double(arc4random_uniform(256))
+        let blue = Double(arc4random_uniform(256))
+        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        return color
     }
 
     
